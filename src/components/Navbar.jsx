039ad88f4281ea9214fd4d8e4f7752ed770fe78e
Koregfx <koregfx@gmail.com>
2022-05-20 21:03:00 +0200

    add Initial Structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import "../styles/navbar.css";
+export function Navbar() {
+  return (
+    <div className="navbar__container">
+      <h1>Navbar</h1>
+    </div>
+  );
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import "../styles/navbar.css";
+
+export function Navbar() {
+  return (
+    <div className="navbar__container">
+      <h1>Navbar</h1>
+    </div>
+  );
+}
